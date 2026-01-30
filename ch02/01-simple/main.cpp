a47c9b75093327e86c7ba05f20294550ebb48c61
SeungYeop Yang <seungyeop.yang@gmail.com>
2026-01-29 19:36:05 -0600

    ch02 squash 2

--- ch02/01-simple/main.cpp
+++ ch02/01-simple/main.cpp
@@ -4,29 +4,30 @@
 
 using namespace std;
 
-int main() {
-  auto age = 98;
-  float num;
-  char letter;
-  string userName;
-  float quotient;
-  int a, b;
+int main()
+{
+    auto age = 98;
+    float num;
+    char letter;
+    string userName;
+    float quotient;
+    int a, b;
 
-  a = 5;
-  b = 2;
-  quotient = a / b;
+    a = 5;
+    b = 2;
+    quotient = a / b;
 
-  cout << quotient << endl;
+    cout << quotient << endl;
 
-  age = 98;
-  num = 4.0;
-  letter = 'm';
-  userName = "Clovis Bagwell";
+    age = 98;
+    num = 4.0;
+    letter = 'm';
+    userName = "Clovis Bagwell";
 
-  cout << "int is " << sizeof(int) << " bytes\n";
-  cout << "Hi, my favorite color" << " is purple" << endl;
-  cout << "and I am " << age << "years old.\n";
-  cout << "User Name is " << userName << endl;
+    cout << "int is " << sizeof(int) << " bytes\n";
+    cout << "Hi, my favorite color" << " is purple" << endl;
+    cout << "and I am " << age << "years old.\n";
+    cout << "User Name is " << userName << endl;
 
-  return 0;
+    return 0;
 }
