c5ccaadc489c0c68418810f1f19f6ff1ba6d8f07
SeungYeop Yang <seungyeop.yang@gmail.com>
2026-02-03 18:37:19 -0600

    ch03

---
+++ ch03/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int age;
+    // string name;
+
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // cin.ignore(100, '\n');
+    // cout << "Enter your name: ";
+    // getline(cin, name);
+
+    // cout << "Hello " << name << ", you are " << age << " years old.\n";
+
+    int a, b;
+    float c;
+
+    a = 5;
+    b = 2;
+    c = a / b;
+
+    cout << "c is " << c << endl;
+
+    return 0;
+}
