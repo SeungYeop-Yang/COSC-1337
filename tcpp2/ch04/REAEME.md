90acf03098d766794a239bf906c4cfe4f8f98a20
SeungYeop Yang <seungyeop.yang@gmail.com>
2026-02-28 00:58:39 -0600

    ch04 02

---
+++ tcpp2/ch04/REAEME.md
@@ -0,0 +1,15 @@
+# Classes
+
+> "Those types are not "abstract"; they are as real as `int` and `float`."
+>
+> &mdash; *Doug McIlroy*
+
+# 4.1
+
+* Concrete classes
+* Abstract classes
+* Classes in class hierarchies
+
+# 4.2 Concrete Types
+
+they behave "just like built-in types."
---
+++ tcpp2/ch04/ch04_02.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/********1*********2*********3*********4*********5*********6*********7********/
+class Complex {
+    double re, im;
+
+public:
+    Complex(double r, double i) : re(r), im(i) {}
+    Complex(double r) : re(r), im(0) {}
+    Complex() : re(0), im(0) {}
+
+    double real() const { return re; }
+    // A C++ method definition suffixed with `const`
+    // const member function promising that the function will not modify
+    // the object's data members.
+
+    void real(double d) { re = d; }
+    double imag() const { return im; }
+    void imag(double d) { im = d; }
+
+    Complex& operator+=(Complex z)
+    {
+        re += z.re;
+        im += z.im;
+        return *this;
+    }
+
+    Complex& operator-=(Complex z)
+    {
+        re -= z.re;
+        im -= z.im;
+        return *this;
+    }
+
+    bool operator==(Complex b) { return re == b.real() && im == b.imag(); }
+};
+
+/********1*********2*********3*********4*********5*********6*********7********/
+
+class Vector {
+private:
+    double* elem;
+    int sz;
+
+public:
+    Vector(int s) : elem(new double[s]), sz(s)
+    {
+        for (int i = 0; i < s; i++)
+            elem[i] = 0;
+    }
+};
+
+int main()
+{
+    Complex a{2.3};
+    Complex b = Complex(2, 3);
+
+    cout << static_cast<string>(a == b ? "True" : "False") << endl;
+    return 0;
+}
