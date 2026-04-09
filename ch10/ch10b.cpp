03f5cf21c9b3c16473702e526498ab14f4b98fd5
SeungYeop Yang <seungyeop.yang@gmail.com>
2026-04-09 18:21:37 -0500

    ch10 session b

--- ch10/ch10b.cpp
+++ ch10/ch10b.cpp
@@ -2,26 +2,15 @@
 
 using namespace std;
 
-void doAddition();
-
 int main()
 {
-    int a;
+    int* intPtr = nullptr;
 
-    doAddition();
+    intPtr = new int;
+    *intPtr = 4;
+
+    cout << intPtr << endl;
+    cout << *intPtr << endl;
 
     return 0;
-}
-
-void doAddition()
-{
-    int x, y;
-
-    cout << "Enter an integer: ";
-    cin >> x;
-
-    cout << "Enter another integer: ";
-    cin >> y;
-
-    cout << "The sum of " << x << " and " << y << " is " << x + y << endl;
 }
